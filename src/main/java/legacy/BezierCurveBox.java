f6d2d10e00d23078a9770c377d89e8a39becc560
tsakov <tsakov.evgeni@gmail.com>
2022-12-28 23:27:53 +0200

    Refactor BezierCurveBox.paint()

--- src/main/java/legacy/BezierCurveBox.java
+++ src/main/java/legacy/BezierCurveBox.java
@@ -7,12 +7,11 @@
  */
 package legacy;
 
-import java.awt.Component;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 
 class BezierCurveBox extends Component {
+
+    private static final int CONTROL_POINT_RADIUS = 5;
 
     Point current = null;
     BezierCurve curve = new BezierCurve();
@@ -27,17 +26,43 @@ class BezierCurveBox extends Component {
 
         curve.evaluate();
 
-        Point p1 = curve.getControlPoints().get(0), p2;
+        paintControlLines(g);
+        paintCurve(g);
+        paintControlPoints(g);
 
-        g.setColor(Color.lightGray);
+        if (!curve.ensure()) {
+            paintWarning(g);
+        }
+    }
+
+    private void paintControlPoints(Graphics g) {
+        g.setColor(Color.BLACK);
+
+        for (Point p : curve.getControlPoints()) {
+            g.fillOval(
+                (int) p.x - CONTROL_POINT_RADIUS,
+                (int) p.y - CONTROL_POINT_RADIUS,
+                CONTROL_POINT_RADIUS * 2,
+                CONTROL_POINT_RADIUS * 2
+            );
+        }
+    }
+
+    private void paintControlLines(Graphics g) {
+        g.setColor(Color.LIGHT_GRAY);
+
+        Point p1 = curve.getControlPoints().get(0), p2;
         for (int i = 1; i < curve.getControlPoints().size(); i++) {
             p2 = curve.getControlPoints().get(i);
             g.drawLine((int) p1.x, (int) p1.y, (int) p2.x, (int) p2.y);
             p1 = p2;
         }
+    }
 
+    private void paintCurve(Graphics g) {
         if (!curve.getEvaluatedCurvePoints().isEmpty()) {
-            g.setColor(Color.red);
+            g.setColor(Color.RED);
+            Point p1, p2;
             p1 = curve.getControlPoints().get(0);
 
             for (int i = 1; i < curve.getEvaluatedCurvePoints().size(); i++) {
@@ -46,15 +71,11 @@ class BezierCurveBox extends Component {
                 p1 = p2;
             }
         }
+    }
 
-        g.setColor(Color.black);
-        for(Point p : curve.getControlPoints())
-            g.fillOval((int) p.x - 5, (int) p.y - 5, 10, 10);
-
-        if (!curve.ensure()) {
-            g.setColor(Color.RED);
-            g.drawString("BAD CURVE!", 5, 15);
-        }
+    private static void paintWarning(Graphics g) {
+        g.setColor(Color.RED);
+        g.drawString("BAD CURVE!", 5, 15);
     }
 
     @Override
